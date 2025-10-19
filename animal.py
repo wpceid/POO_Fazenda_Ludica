a8f968c04386138639c16afc9f226895207dce5a
wpceid <wpceid@gmail.com>
2025-10-19 18:54:41 -0300

    Correções e melhorias no código

--- animal.py
+++ animal.py
@@ -4,34 +4,31 @@ class Animal:
         self.idade = idade
         
     def emitir_som(self):
-        print("O animal emite um som")
+        return "O animal emite um som"
         
     def apresentar(self, nome, idade):
-        print(f"Olá, eu sou {nome} e tenho {idade} anos")
+        return f"Olá, eu sou {nome} e tenho {idade} anos"
         
 class cachorro(Animal):
     def __init__ (self, nome, idade, raca):
-        Animal.nome = nome
-        Animal.idade = idade
+        super().__init__(nome, idade)
         self.raca = raca
         
-    def emitir_som(Animal):
-        print("Au Au!")
+    def emitir_som(self):
+        return "Au Au!"
         
-class gato(Animal):
+class Gato(Animal):
     def __init__ (self, nome, idade, cor_pelo):
-        Animal.nome = nome
-        Animal.idade = idade
+        super().__init__(nome, idade)
         self.cor_pelo = cor_pelo
         
-    def emitir_som(Animal):
-        print("Miau!")
+    def emitir_som(self):
+         return "Miau!"
         
 class vaca(Animal):
     def __init__ (self, nome, idade, producao_leite_litros):
-        Animal.nome = nome
-        Animal.idade = idade
+        super().__init__(nome, idade)
         self.producao_leite_litros = producao_leite_litros
         
-    def emitir_som(Animal):
-        print("Muuuu!")
+    def emitir_som(self):
+        return "Muuuu!"
