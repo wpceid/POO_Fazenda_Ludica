bfa67eb1b4e4009c6ffc9ce19e878a83953578dc
wpceid <wpceid@gmail.com>
2025-10-19 20:13:48 -0300

    Melhorando a performance de chamar os métodos

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ c1 = Cachorro("Rex", 3, "Labrador")
 g1 = Gato("Mimi", 5, "Branco")
 v1 = Vaca("Mimosa", 7, 25.5)
 
-c1.emitir_som()
-g1.emitir_som()
-v1.emitir_som()
-c1.apresentar()
-g1.apresentar()
-v1.apresentar()
+lista_animais = [c1,g1,v1]
+
+for animal in lista_animais:
+    animal.emitir_som()
+    animal.apresentar()
